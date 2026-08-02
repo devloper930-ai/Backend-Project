4528301aafbd928d59c1b2117a55d6152a589686
devloper930-ai <devloper930@gmail.com>
2026-08-02 09:59:59 +0000

    console for test

--- src/middleware/VerifyTokenMiddleware.js
+++ src/middleware/VerifyTokenMiddleware.js
@@ -5,11 +5,13 @@ import config from "../config/config.js";
 
 export async function verifyToken(req, res, next) {
      const token = req.cookies.AuthToken;
+     console.log(token,"verify token midd");
      if (!token) {
           return res.status(401).json({ massage: "unauthrized user" });
      }
      try{
           const verifytoken = await jwt.verify(token, config.JWT_SECRET);
+          console.log(verifytoken,"verifytoken")
           if(!verifyToken){return res.status(401).json({ massage: "invalid token" }) }
           req.id = verifytoken.id;
           next();
