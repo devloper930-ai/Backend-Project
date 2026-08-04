e28156347fc8d5ceffc9970aefde2cc6ecc38f0d
shivam rathod <rathodshivam1943@gmail.com>
2026-08-04 06:09:21 -0600

    clg

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -46,7 +46,11 @@ export async function DeletePost(req, res) {
           }
           const user = await usermodel.findById({_id:userId}, { posrId: 1});
 
+          console.log(userId,"userid");
+          console.log(postId,"postid")
+          console.log(user,"user");
           const isPostExist = user.posts.includes(postId);
+          console.log(isPostExist,"is post exist");
 
           if (!isPostExist) {
                return res.status(403).json({
